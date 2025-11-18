8e398bc2c3dd4d74db938748cf2e3e665eef05fc
Som Shiv Gupta <somg0703@gmail.com>
2025-11-18 19:54:03 +0530

    Data Ingestion has been done

--- demo.py
+++ demo.py
@@ -7,12 +7,17 @@
 #logging.critical("This is a critical message.")
 
 #Below code is to check
-from src.logger import logging
-from src.exception import MyException
-import sys
+# from src.logger import logging
+# from src.exception import MyException
+# import sys
 
-try:
-    a = 1 + 'Z'
-except Exception as e:
-    logging.info(e)
-    raise MyException(e, sys) from e
+# try:
+#     a = 1 + 'Z'
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys) from e
+
+from src.pipeline.training_pipeline import TrainPipeline
+
+pipline = TrainPipeline()
+pipline.run_pipeline()
